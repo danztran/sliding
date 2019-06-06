bab708fe04df97bdd99f9d25d05ca1c538b5a36c
Nguyen An <andinh2601@gmail.com>
2019-06-06 18:36:26 +0700

    add: create event function, fix: sort event with params

---
+++ client/src/components/pieces/TextArea.vue
@@ -0,0 +1,41 @@
+<template>
+	<v-textarea
+		:autofocus="field.autofocus"
+		:type="field.type"
+		:prepend-icon="field.prepend"
+		v-model="field.value"
+		:rules="[fieldRequired(field)]"
+		:error-messages="field.errmsg"
+		:label="$t(field.label)"
+		:rows="field.row || 2"
+		required>
+	</v-textarea>
+</template>
+
+<script>
+export default {
+	name: 'TextArea',
+	props: {
+		field: {
+			type: Object,
+			default() {
+				return {
+					label: 'label',
+					type: 'text',
+					value: '',
+					errmsg: '',
+					prepend: '',
+					rows: null,
+					autofocus: false,
+					required: false
+				};
+			}
+		}
+	},
+	methods: {
+		fieldRequired(field) {
+			return !!this.field.value || !this.field.required || this.$t('requireField');
+		}
+	}
+};
+</script>
