04d2f530dcb0be7df2b0e05de7924fe0ae1d273a
danztran <ntctheboy97@gmail.com>
2019-06-23 13:36:03 +0700

    config.client: access server via LAN in dev mode instead of localhost

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,10 @@
+// configure ip address to access server via LAN in development mode
+const ip = require('ip');
+if (process.env.NODE_ENV === 'development') {
+	const serverHost = `http://${ip.address()}:3000`;
+	process.env.VUE_APP_BASE_URL = serverHost;
+	process.env.VUE_APP_BASE_URL_SOCKET = `${serverHost}/event`;
+}
 
 module.exports = {
 	outputDir: '../server/vue-dist',
