e3720669284f4ed4bca603b8ae940b3f99f4004f
danztran <ntctheboy97@gmail.com>
2019-07-20 11:46:17 +0700

    add: socket event for query and response invited

--- server/sockets/event/event-role-live.js
+++ server/sockets/event/event-role-live.js
@@ -2,6 +2,10 @@ const EventRoleLiveCtlr = requireWrp('controllers/event-role-live-controller');
 
 module.exports = (args) => {
 	const { socket } = args;
+	// query all invited
+	socket.on('query-invited', (info, callback) => EventRoleLiveCtlr.query(args, info, callback));
+	// response to the invite
+	socket.on('response-invited', (info, callback) => EventRoleLiveCtlr.response((args, info, callback)));
 	// add moderator
 	socket.on('add-moderator', (info, callback) => EventRoleLiveCtlr.addModerator(args, info, callback));
 	// remove moderator
