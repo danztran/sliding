d4acb78f00c3adb662f000870ae750b94f8ef539
danztran <ntctheboy97@gmail.com>
2019-08-10 13:02:03 +0700

    fix.client: proxy error

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,11 +2,14 @@ module.exports = {
 	outputDir: '../server/vue-dist',
 	devServer: {
 		port: process.env.PORT || 8080,
-		proxy: {
-			'/': {
-				target: process.env.DOCKER_SERVER_HOST || 'http://localhost:3000',
-			},
-		},
+		proxy: process.env.DOCKER_SERVER_HOST || 'http://localhost:3000',
+		// 	'/': {
+		// 		target: ,
+		// 		// changeOrigin: true,
+		// 		// secure: false,
+		// 	},
+		// 	logLevel: 'silent',
+		// },
 	},
 	chainWebpack: (config) => {
 		config.module
