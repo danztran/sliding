a357285e61fe9e099bb6c5100aaee46cec21e857
danztran <ntctheboy97@gmail.com>
2019-08-19 11:30:54 +0700

    update.server: custom message in api limit middleware

--- server/middlewares/api-limit-middleware.js
+++ server/middlewares/api-limit-middleware.js
@@ -2,30 +2,40 @@ const rateLimit = require('express-rate-limit');
 
 const routes = {
 	signup: rateLimit({
-		windowMs: 15 * 60 * 1000,
+		windowMs: minutes(15),
 		max: 5,
-		message: 'Too many accounts created from this IP, please try again after 15 minutes',
+		message: messages('Too many accounts created from this IP, please try again after some minutes'),
 	}),
 	quickSignup: rateLimit({
-		windowMs: 15 * 60 * 1000,
+		windowMs: minutes(15),
 		max: 5,
-		message: 'Too many accounts created from this IP, please try again after 15 minutes',
+		message: messages('Too many accounts created from this IP, please try again after some minutes'),
 	}),
 	login: rateLimit({
-		windowMs: 10 * 60 * 1000,
+		windowMs: minutes(10),
 		max: 10,
-		message: 'Too many fails login, please try again after 10 minutes',
+		message: messages('Too many fails login, please try again after some minutes'),
 	}),
 	createEvent: rateLimit({
-		windowMs: 15 * 60 * 1000,
+		windowMs: minutes(15),
 		max: 15,
-		message: 'Too many events created, please try again after 15 minutes',
+		message: messages('Too many events created, please try again after 15 minutes'),
 	}),
 	analyticEvent: rateLimit({
-		windowMs: 1 * 60 * 1000,
+		windowMs: minutes(1),
 		max: 10,
-		message: 'Too many analytic requests, please try again after some minutes',
+		message: messages('Too many analytic requests, please try again after some minutes'),
 	}),
 };
+
+function minutes(minute) {
+	return minute * 60 * 1000;
+}
+
+function messages(msg) {
+	return {
+		messages: msg,
+	};
+}
 
 module.exports = routes;
